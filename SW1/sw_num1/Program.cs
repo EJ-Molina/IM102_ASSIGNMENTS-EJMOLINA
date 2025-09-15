7164c1ca8e9c0c16bb8eb0eca0c783583bd87170
EJ Molina <23ur0600@psu.edu.ph>
2025-09-15 12:17:10 +0800

    sw1_(1-2)

---
+++ SW1/sw_num1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sw_num1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter String: ");
+            String str = Console.ReadLine();
+            Console.WriteLine();
+            checkPrime(str);
+            Console.ReadKey();
+        }
+
+        static void checkPrime(String str)
+        {
+            string[] str1 = str.Split(' ');
+            int[] nums = new int[str1.Length];
+            for(int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = int.Parse(str1[i]);
+
+                bool isPrime = true;
+                int num = nums[i]; 
+                int div = 2;
+                if (num < 2)
+                {
+                    isPrime = false;
+                }
+                else{
+                    while (div < num)
+                    {
+                        if (num % div == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                        div++;
+                    }
+                }
+
+                Console.WriteLine($"{i + 1}. {nums[i]}\t {(isPrime ? "Prime" : "Composite")}");
+            }
+        }
+    }
+}
---
+++ SW1/sw_num2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sw_num2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter currency in ($): ");
+            string str = Console.ReadLine();
+            string[] str2 = str.Split(',');
+
+            double[] nums = new double[str2.Length];
+            Console.WriteLine();
+            Console.WriteLine("Dollar($)\tPhil Peso(P)\tJpn Yen (Y): ");
+            for(int i = 0; i < nums.Length; i++)
+            {
+                double num = double.Parse(str2[i]);
+                Console.WriteLine($"{num}\t        {converterD(num).peso}\t        {converterD(num).yen}");
+            }
+        }
+
+        static (double peso, double yen) converterD(double dollar)
+        {
+            double peso = dollar * 57.17;
+            double yen = dollar * 146.67;
+            return (peso, yen);
+        }
+    }
+}
