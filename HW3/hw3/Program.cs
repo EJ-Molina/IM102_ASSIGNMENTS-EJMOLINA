bfda3ea2228ea45e75166ef2e97e8409e96a1c8c
EJ Molina <23ur0600@psu.edu.ph>
2025-10-01 07:12:51 +0800

    hw3

---
+++ HW3/hw3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dictionary<char, int>> wordFreq = new List<Dictionary<char, int>> { };
+            Console.Write("Enter String: ");
+            string str = Console.ReadLine().ToLower();
+            string[] word = str.Split(' ');
+            
+
+            for(int i = 0; i < word.Length; i++)
+            {
+                string nWord = word[i];
+                wordFreq.Add(new Dictionary<char, int>());
+                foreach (char s in nWord)
+                {
+                    if (char.IsLetterOrDigit(s))
+                    {
+                        wordFreq[i][s] = (wordFreq[i].ContainsKey(s) ? wordFreq[i][s] : 0) + 1;
+                    }
+                }
+
+            }
+
+            foreach(var item in wordFreq)
+            {
+                var freqArr = item.Select((kv) => $"{kv.Key}={kv.Value}");
+                string output = string.Join(",", freqArr);
+                Console.WriteLine(output);
+            }
+
+        }
+    }
+}
